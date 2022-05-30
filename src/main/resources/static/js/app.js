15cec657b4c1aa583a16c6608b4ab906c5ab23ff
Enzo Sierra <enzogsierra@gmail.com>
2022-05-30 14:21:00 -0300

    added sweet alert, added movie cover image to movie list, added a button to maximize a movie cover

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function()
 {
+    movieListHandler();
     navLinksHandler();
 });
 
@@ -12,5 +13,32 @@ function navLinksHandler()
     navLinks.forEach(link =>
     {
         if(link.href == window.location.href) link.classList.add("active");
+    });
+}
+
+
+function movieListHandler()
+{
+    const table = document.querySelector("table#movie-list"); // Select table
+    if(!table) return; // If there's no table, exit
+
+    table.addEventListener("click", e => // Add event listener for the whole table
+    {
+        if(e.target.tagName === "IMG" && e.target.id === "movie-thumb") // Check if the element clicked is a movie thumb
+        {
+            const title = e.target.alt;
+            const imageUrl = e.target.src;
+
+            Swal.fire(
+            {
+                background: '#eee',
+                title: title,
+                imageUrl: imageUrl,
+                imageAlt: title,
+                imageWidth: 'auto',
+                imageHeight: 'auto',
+                confirmButtonText: 'Close'
+            });
+        }
     });
 }
