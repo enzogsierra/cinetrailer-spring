50779159bc38f3c89d2ff302dd300f2784661baa
Enzo Sierra <enzogsierra@gmail.com>
2022-05-31 23:03:15 -0300

    backend - find movies by its gender

--- src/main/java/com/example/cinetrailer/controller/PublicController.java
+++ src/main/java/com/example/cinetrailer/controller/PublicController.java
@@ -8,19 +8,14 @@ import com.example.cinetrailer.repository.GenreRepository;
 import com.example.cinetrailer.repository.MovieRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import aj.org.objectweb.asm.Type;
 
 
 @Controller
@@ -43,8 +38,9 @@ public class PublicController
     }
 
     @GetMapping("/movies")
-    public String movies(@RequestParam(defaultValue = "0") String genre, @PageableDefault(sort = "releaseDate", direction = Sort.Direction.DESC, size = 20) Pageable pageable, Model model) throws NumberFormatException
+    public String movies(@RequestParam(defaultValue = "0") String genre, Model model) throws NumberFormatException
     {
+        // Get genre id filter
         Integer genreId = 0;
         try { 
             genreId = Integer.parseInt(genre); 
@@ -53,20 +49,12 @@ public class PublicController
             genreId = 0; 
         }
 
-    
-        //Integer genreId = Integer.parseInt(genre);
-          //  System.out.println("Genre: " + genreId);
-
-
-        //Page<Movie> movies = movieRepository.findAll(pageable);
-        /*if(genre != null)
-        {
-            Integer genreId = Integer.parseInt(genre);
-            System.out.println("Genre: " + genreId);
-        }*/
-            
-        List<Movie> movies = movieRepository.findAll();
+        List<Movie> movies;
         List<Genre> genres = genreRepository.findAll();
+
+        // Get movies list
+        if(genreId == 0) movies = movieRepository.findAll(Sort.by("releaseDate").descending());
+        else movies = movieRepository.findByGenre(genreId);
 
         model.addAttribute("movies", movies);
         model.addAttribute("genres", genres);
